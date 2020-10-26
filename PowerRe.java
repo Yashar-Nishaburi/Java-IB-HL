581cf49c9da40337140b2a18ee4f4550a38356bc
Yashar-Nishaburi <Nishaburiy@gmail.com>
2020-10-26 20:37:35 +0800

    Update Recursion challenges

--- PowerRe.java
+++ PowerRe.java
@@ -1,32 +1,14 @@
 /*
  * PowerRe.java
- * 
- * Copyright 2020 Yashar Nishaburi <yasharnishaburi@10-72-120-145.wifi.8.s.pas.ofs.edu.sg>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
+ * Recursion challenges for HigherLevel Computer science
  * 
  */
 
 
 public class PowerRe 
 {
-
 	static double power(int x, int y)
-	{	
+	{
 		double c;
 		if (y>0)
 		{
@@ -53,18 +35,18 @@ public class PowerRe
 			return 0;
 		}
 	}
-	static int binry(int n)
+	static void binry(int n)
 	{
-		
 		if (n == 0)
 		{
-			return 0;
+			return;
 		}
 		else
 		{
 			int g = n % 2;
+			//return g;
+			binry(n/2);
 			System.out.println(g);
-			return (binry(n/2));
 		}
 	}
 	
