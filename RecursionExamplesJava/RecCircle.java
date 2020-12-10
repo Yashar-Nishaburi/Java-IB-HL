c5d87c7bddbf0ac856a8263af7014f8190174507
Yashar-Nishaburi <Nishaburiy@gmail.com>
2020-12-10 08:59:04 +0800

    WrkSht update

--- RecursionExamplesJava/RecCircle.java
+++ RecursionExamplesJava/RecCircle.java
@@ -20,7 +20,7 @@ public class RecCircle
 			Thread.currentThread().interrupt();
 		}
 	}
-	public static void circledraw(int n, double x, double y, double r)
+	public static void circledraw(int n, double x, double y, double r, int RR, int GG, int BB)
 	{
 		if (n == 1)
 		{
@@ -28,11 +28,16 @@ public class RecCircle
 			{
 			StdDraw.circle(x,y,r);
 			wait(10);
-			circledraw(n,x,y,(r-0.01));
-			
+			//StdDraw.clear();
+			RR = RR + 5;
+			StdDraw.setPenColor(RR,GG,BB);
+			circledraw(n,x,y,(r-0.01),RR,GG,BB);
+			//StdDraw.clear();
+			//RR = 0;
 			}
 			else
 			{
+				StdDraw.setPenColor(0, 0, 0);
 				return;
 			}
 		}
@@ -41,26 +46,31 @@ public class RecCircle
 			if (r > 0) 
 			{
 			//wait(10);
-			circledraw(n,x,y,(r-0.01));
+			circledraw(n,x,y,(r-0.01),RR,GG,BB);
 			}
 			else
 			{
 				return;
 			}
 			StdDraw.circle(x,y,r);
-			wait(10);
+			wait(50);
 		}
 	}
 	public static void main (String[] args) 
 	{
+		int RR = 0;
+		int GG = 0;
+		int BB = 0;
 		int n = IBIO.inputInt("n");
 		double x = 0.5;
 		double y = 0.5;
-		double r = 0.7;
+		double r = 0.5;
 		while (true)
 		{
-			circledraw(n,x,y,r);
+			//RR = 0;
+			circledraw(n,x,y,r,RR,GG,BB);
 			StdDraw.clear();
+			//RR = 0;
 		}
 	}
 }
--- Wrksht/WrkSht34.java
+++ Wrksht/WrkSht34.java
@@ -38,6 +38,11 @@ public class WrkSht34
 	{
 		int i = 1;
 		int x = IBIO.inputInt("Enter a number: ");
+		while (x <= 1)
+		{
+			System.out.println("invalid number ");
+			x = IBIO.inputInt("Enter a number: ");
+		}
 		do
 		{ 
 			i++;
@@ -85,22 +90,150 @@ public class WrkSht34
 		System.out.println("1");
 	}
 //=============================================================================================
-	static void ex5 () //exersice 4.1
+	static void ex5 () //exercise 4.1
 	{
-		int v = IBIO.inputInt(" enter first number ");
-		int b = IBIO.inputInt(" enter second number ");
-		int n = IBIO.inputInt(" Press [1] for multi or [2] for add ");
+		int n = 0;
+		int v = 0;
+		int b = 0;
+		while ( n!= 3 )
+		{
+		v = IBIO.inputInt(" enter first number ");
+		b = IBIO.inputInt(" enter second number ");
+		n = IBIO.inputInt(" Press [1] for multi or [2] for add - [3] quits - ");
+		switch (n) 
+		{
+			case 1:
+				System.out.println(v*b);
+				break;
+			case 2:
+				System.out.println(v+b);
+				break;
+			case 3:
+				System.out.println("~Quit~");
+				break;
+			default:
+				System.out.println("Err-Invalid imput format");
+		}
+		}
+	}
+//=============================================================================================
+	static void ex6 () //exercise 4.2
+	{
+		int sum = 0;
+		int r = 0;
+		for (int i = 1 ; i <= 1000 ; i++)
+		{
+			r = i % 3;
+			switch (r)
+			{
+				case 0:
+					sum = sum + ( i * -5);
+					break;
+				case 1:
+					sum = sum + ( i * 7);
+					break;
+				case 2:
+					sum = sum + ( i * 2);
+					break;
+				default:
+					break;
+			}
+		}
+		System.out.println(sum);
+	}
+//=============================================================================================
+	static void ex7 () //exercise 2.1
+	{
+		int n = IBIO.inputInt("input a number between 50 and 60: ");
+		if (n > 60 || n < 50)
+		{
+			System.out.println("Number was too big / too small");
+		}
+	}
+//=============================================================================================
+	static void ex8 () //exercise 2.2
+	{
+		int n = IBIO.inputInt("Number: ");
+		if ( n % 2 == 0 )
+		{
+			System.out.println("Number is even");
+		}else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+//=============================================================================================
+	static void ex9 () //exercise 2.4
+	{
+		for ( int i = 1 ; i <=10 ; i++)
+		{
+			System.out.print(i + "- " + i*i + " ~~ ");
+		}
+		System.out.println("");
+	}
+//=============================================================================================
+	static void ex10 () //exercise 2.5
+	{
+		for ( int i = 1 ; i <= 100 ; i++)
+		{
+			if ( i%2 == 0 )
+			{
+				System.out.print(i + "~~");
+			}
+		}System.out.println("");
+	}
+//=============================================================================================
+	static void ex11 () //exercise 2.6+2.7
+	{
+		for ( int i = 1 ; i <= 100 ; i++)
+		{
+			if ( i%2 == 0 && i%3 == 0)
+			{
+				System.out.print(i + "~~");
+			}
+		}System.out.println("");
+	}
+//=============================================================================================
+	static void ex12 () //exercise 2.8
+	{
+		int counter = 0;
+		for ( int i = 1 ; i <= 1000000 ; i ++)
+		{
+			if (i%2 !=0 && i%3 !=0 && i%5 !=0 && i%7 !=0)
+			{
+				counter++;
+			}
+		}System.out.println("Counter: " + counter);
 	}
 //=============================================================================================
 //=============================================================================================
 //=============================================================================================
 	public static void main (String[] args) 
 	{
+		System.out.println("//exercise 3.1");
 		ex1();
+		System.out.println("//exercise 3.2");
 		ex2();
+		System.out.println("//exercise 3.3");
 		ex3();
+		System.out.println("//exercise 3.4");
 		ex4();
+		System.out.println("//exercise 4.1");
 		ex5();
+		System.out.println("//exercise 4.2");
+		ex6();
+		System.out.println("//exercise 2.1");
+		ex7();
+		System.out.println("//exercise 2.2");
+		ex8();
+		System.out.println("//exercise 2.4");
+		ex9();
+		System.out.println("//exersice 2.5");
+		ex10();
+		System.out.println("//exersice 2.6+2.7");
+		ex11();
+		System.out.println("//exercise 2.8 - Question header wrong (should be 2 and 3 and 5 and 7*");
+		ex12();
 	}
 }
 
